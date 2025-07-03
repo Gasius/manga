95a6abb6d6e40dff39ebcb46b92a025e71387812
Your Name <you@example.com>
2025-07-03 22:08:32 +0300

    дохуя графики шапки сайта

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,11 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\MainController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::middleware(['auth'])->group(function () {
-    Route::inertia('/', 'Index');
+Route::get('/', [MainController::class, 'index'])->name('main.index');
 
-});
 
-Route::prefix('/auth')->group(function () {
-    Route::middleware(['guest'])->group(function () {
-        Route::get('/login', [AuthController::class, 'show'])->name('auth.login');
-        Route::get('/register', [AuthController::class, 'showRegister'])->name('auth.register');
-    });
-});
 
